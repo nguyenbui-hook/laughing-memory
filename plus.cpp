f9e92b9963c79e067182356247c5641fe822aeac
spartan-nguyenbui <nguyen.bui@c0x12c.com>
2024-09-12 18:35:56 +0700

    update

--- plus.cpp
+++ plus.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
-    // Correct logic: adding the two numbers
-    result = num1 + num2;
+    // Intentional bug: subtracting instead of adding
+    result = num1 - num2;
 
     cout << "The sum is: " << result << endl;
 
